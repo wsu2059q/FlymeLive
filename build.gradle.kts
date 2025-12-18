9d896dae590de2af5aa884d7b5038b1c4f32bf77
wsu2059q <wsu2059@qq.com>
2025-12-18 23:36:15 +0800

    添加 Maven 发布配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
+
+import java.net.URI
 
 android {
     namespace = "com.erisdev.flymelive"
@@ -32,6 +35,7 @@ android {
     }
     buildFeatures {
         aidl = true
+        buildConfig = true
     }
 }
 
@@ -41,4 +45,62 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components.findByName("android") ?: components.findByName("release"))
+                
+                groupId = "com.erisdev"
+                artifactId = "flymelive"
+                version = "1.0.0"
+                
+                pom {
+                    name.set("FlymeLive")
+                    description.set("一个用于简化 Flyme 系统实况通知功能使用的库")
+                    url.set("https://github.com/wsu2059q/FlymeLive")
+                    
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    
+                    developers {
+                        developer {
+                            id.set("wsu2059q")
+                            name.set("WSu2059")
+                            email.set("wsu2059@qq.com")
+                        }
+                    }
+                    
+                    scm {
+                        connection.set("scm:git:git://github.com/wsu2059q/FlymeLive.git")
+                        developerConnection.set("scm:git:ssh://github.com:wsu2059q/FlymeLive.git")
+                        url.set("https://github.com/wsu2059q/FlymeLive/tree/main")
+                    }
+                }
+            }
+        }
+        
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/wsu2059q/FlymeLive")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
+            
+            // 测试
+            maven {
+                name = "Local"
+                url = uri("${project.layout.buildDirectory.get()}/repo")
+            }
+        }
+    }
 }
